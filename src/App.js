2b3a39a028c54f90220fd95d8a21798fb700542a
Daniel Petrie <danielpetrie07@gmail.com>
2023-08-31 15:13:35 +1200

    removed unused vars

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './App.css'
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Home from './pages'
-import SigninPage from './pages/signin'
 
 function App() {
   return (
